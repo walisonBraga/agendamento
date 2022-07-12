6c2260567431bb7daad933571da06c4d5fa55b88
walisonBraga <81987186+walisonBraga@users.noreply.github.com>
2022-07-12 13:51:49 -0500

    Update app.module.ts

--- barbearia/src/app/app.module.ts
+++ barbearia/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhotoComponent } from './photos/photo/photo.Component';
+import { PhotosModule } from './photos/photos.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+      AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+      BrowserModule,
+      PhotosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
