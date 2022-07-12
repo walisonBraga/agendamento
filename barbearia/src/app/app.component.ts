f0e86dd69d9f95da18401b9bcecef3798751cbfb
walisonBraga <81987186+walisonBraga@users.noreply.github.com>
2022-07-12 13:51:47 -0500

    Update app.component.ts

--- barbearia/src/app/app.component.ts
+++ barbearia/src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'barbearia';
+  photos = [
+    {
+    url: 
+    'https://www.vero.com.br/wp-content/uploads/2017/05/coriolano.png', 
+    description: 'Barbearia'
+  },
+  {
+    url:
+    'https://radioriodejaneiro.digital/wp-content/uploads/2019/12/barbeiro.jpg',
+     description: 'Homem'
+  },
+  {
+    url:
+    'https://protectionforbeauty.volkdobrasil.com.br/blog/wp-content/uploads/2019/12/GettyImages-1158457201.jpg',
+     description: 'Mulher'
+  }
+
+];
+
 }
