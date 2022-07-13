ab5debfffa9038e0a49f12c4f0a2d3a34b512203
walisonBraga <81987186+walisonBraga@users.noreply.github.com>
2022-07-13 07:34:00 -0500

    Update app.component.ts

--- barbearia/src/app/app.component.ts
+++ barbearia/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+
 import { PhotoService } from './photos/photo/photo.service';
 
 @Component({
